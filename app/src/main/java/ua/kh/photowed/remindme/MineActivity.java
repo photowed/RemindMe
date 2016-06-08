5a229627bee18d00d059875f961aa4517bbc4215
photowed <photowed@i.ua>
2016-06-08 14:56:48 +0300

    Create base stucture more..

---
+++ app/src/main/java/ua/kh/photowed/remindme/MineActivity.java
@@ -0,0 +1,17 @@
+package ua.kh.photowed.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by deSad on 08.06.2016.
+ */
+public class MineActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
