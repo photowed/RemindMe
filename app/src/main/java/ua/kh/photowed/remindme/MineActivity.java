e19f555b3e038bc75c0726b48f7ca7309e6d3b12
photowed <photowed@i.ua>
2016-06-08 16:19:48 +0300

    Add toolbar

--- app/src/main/java/ua/kh/photowed/remindme/MineActivity.java
+++ app/src/main/java/ua/kh/photowed/remindme/MineActivity.java
@@ -2,16 +2,35 @@ package ua.kh.photowed.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by deSad on 08.06.2016.
  */
 public class MineActivity extends Activity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 
 }
